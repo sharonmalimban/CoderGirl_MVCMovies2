0718f232314d70a0834b9157d9b9fe5a657de0a3
dwmoeller1 <dwmoeller1@aol.com>
2019-04-16 22:44:17 -0500

    Instructions. Tests partially complete.

---
+++ CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoderGirl_MVCMovies.Controllers
+{
+    public class MovieRatingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Create a string html template for a form
+        // with method of post
+        // an input with name "movieName" 
+        // a select with name "rating" and that contains options for 1 through 5
+        // a button with type of submit
+
+        // Create an GET Action for Create which returns the above template as Content
+
+        // Create a POST Action for Create which takes...
+        // two string parameters which match the names of the input and select names in the template
+        // The method should redirect to a the GET Action for Details. Be sure to pass the parameters as route values.
+
+        //Create a GET Action for Details
+        // Details should take the parameters passed by the POST Action for Create
+        // Details should return a string as Content. This string should be in the format "{moveName} has a rating of {rating}"
+    }
+}
---
+++ Test/TestMovieRatingController.cs
@@ -0,0 +1,91 @@
+using HtmlAgilityPack;
+using System;
+using Xunit;
+using CoderGirl_MVCMovies.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Test
+{
+    public class TestMovieRatingController
+    {
+        private MovieRatingController controller;
+
+        public TestMovieRatingController()
+        {
+            controller = new MovieRatingController();
+        }
+
+        [Fact]
+        public void Test_GET_Create_ContainsForm()
+        {           
+            ContentResult result = (ContentResult)controller.Create();
+
+            Assert.Equal("text/html", result.ContentType);
+            HtmlNode html = HtmlNode.CreateNode(result.Content);
+            Assert.Equal("form", html.Name);
+        }
+
+        [Fact]
+        public void Test_GET_Create_ContainsForm_WithMethodOfPost()
+        {
+            ContentResult result = (ContentResult)controller.Create();
+
+            HtmlNode html = HtmlNode.CreateNode(result.Content);
+            HtmlAttribute method = html.Attributes.FirstOrDefault(attr => attr.Name.ToLower() == "method");
+            Assert.NotNull(method);
+            Assert.Equal("post", method.Value.ToLower());
+        }
+
+        [Fact]
+        public void Test_GET_Create_ContainsForm_WithNoAction_OrActionForCreateMovieRating()
+        {
+            ContentResult result = (ContentResult)controller.Create();
+
+            HtmlNode html = HtmlNode.CreateNode(result.Content);
+            HtmlAttribute formAction = html.Attributes.FirstOrDefault(attr => attr.Name.ToLower() == "action");
+            Assert.True(formAction == null || formAction.Value.ToLower() == "/MovieRating/Create");
+        }
+
+        [Fact]
+        public void Test_GET_Create_ContainsForm_WithInputElement()
+        {
+            ContentResult result = (ContentResult)controller.Create();
+
+            HtmlNode input = HtmlNode.CreateNode(result.Content).ChildNodes.FirstOrDefault(node => node.Name == "input");
+            Assert.NotNull(input);
+            Assert.Equal("movieName", input.Attributes.SingleOrDefault(attr => attr.Name == "name").Value);
+        }
+
+        [Fact]
+        public void Test_GET_Create_ContainsForm_WithSelectElement_ContainingRatings()
+        {
+
+        }
+
+        [Fact]
+        public void Test_GET_Create_ContainsForm_WithSubmitButton()
+        {
+
+        }
+
+        [Theory]
+        [InlineData("Star Wars", "5")]
+        [InlineData("The Princess Bride", "4")]
+        public void Test_POST_Create_RedirectsTo_GETDetails_WithInputValues(string movieName, string rating)
+        {
+
+        }
+
+        [Theory]
+        [InlineData("Star Wars", "5")]
+        [InlineData("The Princess Bride", "4")]
+        public void Test_GET_Details(string movieName, string rating)
+        {
+            string expectedString = $"{movieName} has a rating of {rating}.";
+
+            
+
+        }
+    }
+}
