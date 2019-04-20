09648cea4ece3f53d76448b5967743bc2f74168e
dwmoeller1 <dwmoeller1@aol.com>
2019-04-20 14:37:04 -0500

    Solution to week 13

--- CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
@@ -8,25 +8,40 @@ namespace CoderGirl_MVCMovies.Controllers
 {
     public class MovieRatingController : Controller
     {
+        private string htmlForm = @"
+            <form method='post'>
+                <input name='movieName' />
+                <select name='rating'>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>                    
+                </select>
+                <button type='submit'>Rate it</button>
+            </form>";
+
         public IActionResult Index()
         {
             return View();
         }
 
-        //Create a string html template for a form
-        // with method of post
-        // an input with name "movieName" 
-        // a select with name "rating" and that contains options for 1 through 5
-        // a button with type of submit
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return Content(htmlForm, "text/html");
+        }
 
-        // Create an GET Action for Create which returns the above template as Content
+        [HttpPost]
+        public IActionResult Create(string movieName, string rating)
+        {
+            return RedirectToAction(actionName: nameof(Details), routeValues: new { movieName, rating });
+        }
 
-        // Create a POST Action for Create which takes...
-        // two string parameters which match the names of the input and select names in the template
-        // The method should redirect to a the GET Action for Details. Be sure to pass the parameters as route values.
-
-        //Create a GET Action for Details
-        // Details should take the parameters passed by the POST Action for Create
-        // Details should return a string as Content. This string should be in the format "{moveName} has a rating of {rating}"
+        [HttpGet]
+        public IActionResult Details(string movieName, string rating)
+        {
+            return Content($"{movieName} has a rating of {rating}");
+        }
     }
 }
