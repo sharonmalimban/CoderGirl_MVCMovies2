f094227a9cd4b3dbf3a884a393eb16dfca317af1
dwmoeller1 <dwmoeller1@aol.com>
2019-04-21 23:05:35 -0500

    UAT for create movie rating

--- Test/TestMovieRatingViews.cs
+++ Test/TestMovieRatingViews.cs
@@ -7,12 +7,14 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Reflection;
 using System.IO;
+using OpenQA.Selenium.Support.UI;
 
 namespace Test
 {
     public class TestMovieRatingViews : IDisposable
     {
         private ChromeDriver driver;
+        private const string BASE_URL = "http://localhost:59471/movierating";
 
 
         public TestMovieRatingViews()
@@ -20,14 +22,28 @@ namespace Test
             driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
         }
 
-        [Fact]
-        public void TestCreate()
+        [Theory]
+        [InlineData("Star Wars", "5")]
+        [InlineData("Princess Bride", "4")]
+        public void TestCreate(string name, string rating)
         {
-            driver.Navigate
+            driver.Url = BASE_URL + "/create";
+            var source = driver.PageSource;
+            var submit = driver.FindElementByTagName("button");
+            Assert.Equal("submit", submit.GetAttribute("type"));
+            var nameInput = driver.FindElementByName("movieName");
+            var ratingInput = new SelectElement(driver.FindElementByName("rating"));
+
+            nameInput.SendKeys(name);
+            ratingInput.SelectByText(rating);
+            submit.Click();
+
+            Assert.Equal(Uri.EscapeUriString(BASE_URL + $"/details?movieName={name}&rating={rating}"), driver.Url, true);
         }
 
         public void Dispose()
         {
+            driver.Close();
             driver.Dispose();
         }
     }
