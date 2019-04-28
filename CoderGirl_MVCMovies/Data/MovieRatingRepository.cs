07098fd8e49268d78bec321705750b1046df5877
sebudwell@gmail.com <sebudwell@gmail.com>
2019-04-28 18:40:15 -0500

    first

---
+++ CoderGirl_MVCMovies/Data/MovieRatingRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderGirl_MVCMovies.Data
+{
+    public class MovieRatingRepository : IMovieRatingRepository
+    {
+        public decimal GetAverageRatingByMovieName(string movieName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<int> GetIds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetMovieNameById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetRatingById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveRating(string movieName, int rating)
+        {
+            
+        }
+    }
+}
